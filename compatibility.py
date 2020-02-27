c5e2b6ec321ee373a07ed62b6fb5653498c14a56
Mikayla Campbell <hewantshiscar@dyn-10-108-70-150.wless.uoregon.edu>
2020-02-27 15:33:06 -0800

    Created algorithm file

---
+++ compatibility.py
@@ -0,0 +1,53 @@
+"""
+CIS 422 Project 2 : Compatibility Algorithm File
+
+Last Modified: 2/27/20
+
+Authors: Mikayla Campbell, Bethany Van Meter
+"""
+
+class User:
+	"""
+
+	"""
+	 def __init__(self, user_type, first, last, age, gender, questionnaire, bio, email):
+	 	self.user_type = user_type # int (Mentor = 1, mentee = 0)
+	 	self.first = first # string
+	 	self.last = last # string
+	 	self.age = age # string
+	 	self.gender = gender # string
+	 	self.questionnaire = q # list
+	 	# Up to 4 choices multiple choice quesions
+	 	# 25 questions
+	 	self.bio = bio # string
+	 	self.email = email # string
+	 	self.user_matches = Matches()
+
+
+class Matches:
+	"""
+
+	"""
+	def __init__(self, matches):
+		# key = user
+		# value = % compatible
+		self.matches = matches # dictionary
+
+
+def pref_check():
+	"""
+
+	"""
+	pass
+
+def compat():
+	"""
+
+	"""
+	pass
+
+def main():
+	"""
+	Test it
+	"""
+	return 1
