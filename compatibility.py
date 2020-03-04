f2ec1e19836fdaec948c1287ce4178d0b7e9806d
Mikayla Campbell <hewantshiscar@Mikaylas-MBP.lan>
2020-03-03 19:59:36 -0800

    Login credential checker function created

--- compatibility.py
+++ compatibility.py
@@ -44,14 +44,23 @@ class User:
 			self.username, self.password)
 
 
-def equal_q_answer(current_user, user, q_num, matches):
+def login_check(username, password):
+	"""Checks login credentials"""
+	for user in users:
+		if user.username == username and user.password == password:
+			current_user = user
+			return 1
+	return 0
+
+
+def equal_q_answer(user, q_num, matches):
 	"""Mentor and mentee must have equal answers"""
 	percent = abs(current_user.q[q_num] - user.q[q_num])
 	real_percent = percent_scale[percent]
 	matches[user] += real_percent
 
 
-def equal_or_higher(current_user, user, q_num, matches):
+def equal_or_higher(user, q_num, matches):
 	"""Mentor needs to be 1 or higher than mentee or equal"""
 	percent = current_user.q[q_num] - user.q[q_num]
 	if percent >= 0:
@@ -59,7 +68,7 @@ def equal_or_higher(current_user, user, q_num, matches):
 		matches[user] += real_percent
 
 
-def equal_or_lower(current_user, user, q_num, matches):
+def equal_or_lower(user, q_num, matches):
 	"""Mentee needs to be 1 or lowerer than mentor or equal"""
 	percent = current_user.q[q_num] - user.q[q-num]
 	if percent <= 0:
@@ -67,7 +76,7 @@ def equal_or_lower(current_user, user, q_num, matches):
 		matches[user] += real_percent
 
 
-def higher(current_user, user, q_num, matches):
+def higher(user, q_num, matches):
 	"""Mentor needs to be 1 or higher than mentee"""
 	percent = current_user.q[q_num] - user.q[q_num]
 	if percent > 0:
@@ -75,7 +84,7 @@ def higher(current_user, user, q_num, matches):
 		matches[user] += real_percent
 
 
-def lower(current_user, user, q_num, matches):
+def lower(user, q_num, matches):
 	"""Mentee needs to be 1 or lower than mentor"""
 	percent = current_user.q[q_num] - user.q[q_num]
 	if percent < 0:
@@ -83,7 +92,7 @@ def lower(current_user, user, q_num, matches):
 		matches[user] += real_percent
 
 
-def pref_check(current_user):
+def pref_check():
 	"""Remove users who do not fit the current user's preferenes
 
 	0) Gender (Male, Female, Other)
@@ -106,7 +115,7 @@ def pref_check(current_user):
 	return pref_matches
 
 
-def compat(current_user, matches):
+def compat(matches):
 	"""Compute compatibility amongst users"""
 
 	# If the current user is a mentor
@@ -216,8 +225,8 @@ def compat(current_user, matches):
 # 	users.append(user4)
 # 	users.append(user5)
 
-# 	current_matches = pref_check(current_user)
-# 	compat(current_user, current_matches)
+# 	current_matches = pref_check()
+# 	compat(current_matches)
 
 # 	count = 0
 # 	for i in current_user.matches:
